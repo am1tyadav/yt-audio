67803358cf9fe6e1fea6c128db9a223920b55081
Amit Yadav <amit.yadav.iitr@gmail.com>
2022-01-06 09:13:34 +0000

    keep track of updated data

--- yt_audio/__init__.py
+++ yt_audio/__init__.py
@@ -1,9 +1,13 @@
 # Compose a data ingestion pipeline
 import json
 import os
+import warnings
 from yt_audio.utils import yid_to_url
 from yt_audio.download import download_audio
 from yt_audio.process import process_audio
+
+
+warnings.simplefilter("ignore")
 
 
 def ingest(data_file: str, sample_rate: int, output_dir: str):
@@ -17,6 +21,12 @@ def ingest(data_file: str, sample_rate: int, output_dir: str):
     with open(data_file, "r") as f:
         dataset = json.load(f)
 
+    num_examples = len(dataset["id"])
+
+    if "processed" not in dataset:
+        dataset["processed"] = [False] * num_examples
+        dataset["file_path"] = [""] * num_examples
+
     raw_audio_dir = os.path.join(output_dir, "raw")
     processed_audio_dir = os.path.join(output_dir, "processed")
 
@@ -24,6 +34,9 @@ def ingest(data_file: str, sample_rate: int, output_dir: str):
     os.makedirs(processed_audio_dir, exist_ok=True)
 
     for index , _id in enumerate(dataset["id"]):
+        if dataset["processed"][index]:
+            continue
+
         _url = yid_to_url(_id)
         _label = dataset["label"][index]
         _start = dataset["start"][index]
@@ -35,9 +48,18 @@ def ingest(data_file: str, sample_rate: int, output_dir: str):
             chunk_file_path = process_audio(file_path, processed_audio_dir, _label,
                                             sample_rate, _start, _end)
             print(f"Created file {chunk_file_path}")
+            dataset["file_path"][index] = chunk_file_path
         else:
             print(f"Skipping id {_id} - could not be downloaded from YouTube")
         
         if stop_process:
             print("Stopping process now")
             break
+
+        # Irrespective of the result, file was still processed
+        dataset["processed"][index] = True
+
+    # Overwrite updated file
+    with open(data_file, "w") as f:
+        print("Saving updated file")
+        json.dump(dataset, f)
